b5696404558472d5fe29f48fbe2f474f4dab8950
saikirangadupathi <141751890+saikirangadupathi@users.noreply.github.com>
2024-09-18 00:52:09 +0530

    Update cancellationRecords.js

--- model/cancellationRecords.js
+++ model/cancellationRecords.js
@@ -2,28 +2,25 @@ const mongoose = require('mongoose');
 
 const CancellationLogSchema = new mongoose.Schema({
   orderID: {
-    type: mongoose.Schema.Types.ObjectId, // Reference to the Order document
-    required: true,
-    ref: 'Order', // Reference to the Order collection
+    type: String, // Reference to the Order document
+    required: false,
   },
   sellerId: {
-    type: mongoose.Schema.Types.ObjectId, // Reference to the Seller (Customer) document
-    required: true,
-    ref: 'Customer', // Reference to the Customer collection
+    type: String, // Reference to the Seller (Customer) document
+    required: false,
   },
   buyerId: {
-    type: mongoose.Schema.Types.ObjectId, // Reference to the Scrap Buyer document
+    type: String, // Reference to the Scrap Buyer document
     required: false, // This may not exist if the cancellation is by the customer before a buyer is assigned
-    ref: 'ScrapBuyer', // Reference to the ScrapBuyer collection
   },
   cancelledBy: {
     type: String,
-    enum: ['Customer', 'ScrapBuyer'], // To store who initiated the cancellation
+    enum: ['Customer', 'ScrapBuyer' ,'DeliveryAgent'], // To store who initiated the cancellation
     required: true,
   },
   cancellationReason: {
     type: String, // Reason for the cancellation
-    required: true,
+    required: false,
     trim: true,
   },
   cancellationDate: {
@@ -33,18 +30,19 @@ const CancellationLogSchema = new mongoose.Schema({
   orderType: {
     type: String, // Type of the order (e.g., 'Pickup', 'Drop-off')
     required: true,
-    enum: ['Pickup', 'Drop-off'], // Ensuring it follows predefined types
+    enum: ['Pickup', 'DropOff'], // Ensuring it follows predefined types
   },
   statusBeforeCancellation: {
     type: String, // Status of the order before cancellation
-    required: true,
-    enum: ['Pending', 'Accepted', 'In Progress', 'Completed'], // Various possible statuses before cancellation
+    required: false,
+    enum: ['Pending', 'Accepted', 'In Progress'], // Various possible statuses before cancellation
   },
   previousStatusDate: {
     type: Date, // When the order was in the previous status before cancellation
-    required: true,
+    required: false,
   },
 });
 
-module.exports = mongoose.model('CancellationLog', CancellationLogSchema);
+const CancellationsLog = mongoose.model('CancellationLog', CancellationLogSchema);
 
+module.exports = CancellationsLog;
